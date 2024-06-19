b0c0f2212e827274652393420d902b43681da573
Matthew Mox <matttmox54@gmail.com>
2024-06-19 19:46:12 -0400

    Initial Release

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { Stack } from "expo-router";
+import { SQLiteProvider, type SQLiteDatabase } from 'expo-sqlite';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
+  const initializeDB = async (db: SQLiteDatabase) => {
+    await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS drink_types (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);
+      CREATE TABLE IF NOT EXISTS drinks_drank (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp TEXT NOT NULL, drink_type INTEGER NOT NULL);
+    `)
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <SQLiteProvider databaseName="data.db" onInit={initializeDB}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" options={{title: "Drink Tracker"}}/>
+        <Stack.Screen name="drinkSelection" options={{title: "Select Drink"}}/>
+        <Stack.Screen name="newDrinkType" options={{title: "Enter Drink Info"}}/>
       </Stack>
-    </ThemeProvider>
+    </SQLiteProvider>
+    
   );
 }
